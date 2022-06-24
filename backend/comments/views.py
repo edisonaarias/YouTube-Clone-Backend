156b49d0e5e17d72225cf079fa81ea88846eb19f
unknown <edisonalexanderarias@gmail.com>
2022-06-24 14:59:48 -0700

    Created a post for Postman + added 400 and 201 status

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -3,11 +3,16 @@ from rest_framework.response import Response
 from .serializers import CommentSerializer
 from .models import Comment
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def comments_list(request):
-
-    comments = Comment.objects.all()
-
-    serializer = CommentSerializer(comments, many=True)
-
-    return Response(serializer.data)
+    if request.method == 'GET':
+        comments = Comment.objects.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response (serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
