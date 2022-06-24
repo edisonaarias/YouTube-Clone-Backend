e178f63521ced22ceef471c8f0961627a8c98d3c
unknown <edisonalexanderarias@gmail.com>
2022-06-24 12:42:03 -0700

    Added the serializers file to my comments folder

---
+++ backend/comments/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Comment
+
+class CommentSerializer(serializers.ModSerializer):
+    class Meta:
+        model = Comment
+        # fields =
