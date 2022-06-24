6d2c84d76c6504cf0eb1e4f9db002796a3d4b9e6
unknown <edisonalexanderarias@gmail.com>
2022-06-24 10:01:06 -0700

    Added models and migrated models to create database

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from authentication.models import User
 
 # Create your models here.
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = models.CharField(max_length=50)
+    text = models.CharField(max_length=255)
+    like = models.IntegerField(max_length=255)
+    dislikes = models.IntegerField(max_length=255)
--- backend/drf_jwt_backend/settings.py
+++ backend/drf_jwt_backend/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'authentication.apps.AuthenticationConfig',
     'corsheaders',
-    'cars.apps.CarsConfig'  # This app is for example use only
-    # 'comments',
+    'cars.apps.CarsConfig',  # This app is for example use only
+    'comments',
 ]
 
 MIDDLEWARE = [
