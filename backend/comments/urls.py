37e71c72ef9217067b429632304ff868bf082c8c
unknown <edisonalexanderarias@gmail.com>
2022-06-26 16:19:14 -0700

    Added a function to get a comment by its id + added an in<pk>/ to the url

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.comments_list),
+    path('<int:pk>/', views.Comment_detail),
 ]
--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,8 +1,10 @@
+import comments
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import CommentSerializer
 from .models import Comment
+from comments import serializers
 
 @api_view(['GET','POST'])
 def comments_list(request):
@@ -16,3 +18,13 @@ def comments_list(request):
         serializer.save()
         return Response (serializer.data, status=status.HTTP_201_CREATED)
   
+@api_view(['GET'])
+def Comment_detail(request, pk):
+    try:
+        comment = Comment.objects.get(pk=pk)
+        serializers = CommentSerializer(comment);
+        return Response(serializers.data)
+        
+    except Comment.DoesNotExist:
+        return Response(status=status.HTTP_404);
+
